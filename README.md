7155bb88f6a44fdbe507ed5552ed86b3ddc7b489
Luigi210 <akhmetov.asan@gmail.com>
2021-02-27 14:57:52 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-z5xu7f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-z5xu7f)
---
+++ src/app/products.ts
@@ -0,0 +1,132 @@
+export const products = [
+  {
+    id: 1,
+    name: "Samsung Galaxy S10Lite 128GB Black",
+    price: 603000,
+    description:
+      "For a long time Samsung did not want to release lightweight versions of its flagships, the A-series models were a conditional replacement, but they did not compete directly with the company's flagships either in cost or in positioning. In 2020, Samsung is trying a new strategy, namely, expanding the lineup by semi-flags or lightweight Lite versions, when the buyer can choose a model with good performance, but simplified characteristics.",
+    rating: "4.0/5.0",
+    image:
+      "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/8/c/8c1768745d32e1c3c1bb62cab529f1803c8160aa_215405_3kjh.jpg",
+    link:
+      "https://www.technodom.kz/smartfony-i-gadzhety/smartfony-i-telefony/smartfony/p/smartfon-samsung-galaxy-s10-lite-128gb-blue-config/color-palette/chernihy/volume/128-gb"
+  },
+  {
+    id: 2,
+    name: "Samsung Galaxy A51 64GB Black",
+    price: 313.3,
+    description:
+      "The Galaxy A51's borderless display optimizes visual symmetry. Now you can play, watch movies, surf the net and multitask without interruption on the bright 6.5-inch sAMOLED screen with FHD + resolution. Immerse yourself in content with a slim display bezel and maximum usable space.",
+    rating: "4.1/5.0",
+    image:
+      "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/d/4/d4c43c512412ccb0b36dc74135fc3095303c84cb_213585_2as.jpg",
+    link:
+      "https://www.technodom.kz/smartfony-i-gadzhety/smartfony-i-telefony/smartfony/p/smartfon-gsm-samsung-galaxy-a51-128gb-black-config/color-palette/chernihy/volume/64-gb"
+  },
+  {
+    id: 3,
+    name: "Samsung Galaxy A11 32GB Black",
+    price: 156.65,
+    description:
+      "The Galaxy A11 model is in demand among those who meticulously choose a smartphone in the store, trust the Samsung brand, but at the same time do not do their homework, do not research the offer in the segment. The logic of buyers is simple - there is a model A01, which is much cheaper. In the eyes of buyers, the older device is better in every sense, and therefore it is worth focusing on it. Sales prove it, in Russia A11 is among the five best-selling smartphones.",
+    rating: "3.0/5.0",
+    image:
+      "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/4/7/47c6f1dcb09efc08c1b2d2450499f4d7589caeb9_219516_1.jpg",
+    link:
+      "https://www.technodom.kz/smartfony-i-gadzhety/smartfony-i-telefony/smartfony/p/smartfon-samsung-galaxy-a11-32gb-black-config/color-palette/chernihy/volume/32-gb"
+  },
+  {
+    id: 4,
+    name: "Samsung Galaxy A31 64GB Black",
+    price: 265.1,
+    description:
+      "When there is a super popular model in the lineup, which is widespread in all sales channels, this creates a number of problems for the manufacturer. The first problem is popularity and sales volumes, which force a number of players to reduce prices at their own expense, this becomes their marketing ploy to attract buyers. And here we come to a paradoxical situation: the older and most popular model A51 is more profitable in cost than the outgoing A31, which is lower in positioning. Formally, if you look at the difference between the devices, it is somewhat cosmetic, in something serious.",
+    rating: "5.0/5.0",
+    image:
+      "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/0/d/0d3b4837a905f3f17c1b3386fbdba172145cf7c5_217129_1xxc.jpg",
+    link:
+      "https://www.technodom.kz/smartfony-i-gadzhety/smartfony-i-telefony/smartfony/p/smartfon-samsung-galaxy-a31-64gb-white-config/color-palette/chernihy/volume/64-gb"
+  },
+  {
+    id: 5,
+    name: "Apple iPhone 11 128GB Black",
+    price: 939.89,
+    description:
+      "As part of our efforts to reach our environmental goals, iPhone 11 does not include a power adapter or EarPods. Included in the box is a USB‑C to Lightning cable that supports fast charging and is compatible with USB‑C power adapters and computer ports.",
+    rating: "5.0/5.0",
+    image:
+      "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/3/5/353535e48550691ecf412838d844026d3307ab12_228942_1.jpg",
+    link:
+      "https://www.technodom.kz/smartfony-i-gadzhety/smartfony-i-telefony/smartfony/p/smartfon-apple-iphone-11-128gb-purple-config/color-palette/chernihy/volume/128-gb"
+  },
+  {
+    id: 6,
+    name: "Samsung Galaxy A02s 32GB Black",
+    price: 144.6,
+    description:
+      "Smartphone of 2020 release. It measures 164.2 x 75.9 x 9.1 and weighs 196 g. It features a 6.5 PLS IPS display. The resolution is 720 x 1600 pixels, the pixel density is 270 ppi. For selfies and video calls, one 5 MP pre-camera is responsible. photo and video available Triple rear camera 13 MP.Processor has Octa-core 1.8 GHz Cortex-A53 and memory 32GB 3GB RAM, 64GB 4GB RAM.Battery capacity 5000 mAh.",
+    rating: "4.8/5.0",
+    image:
+      "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/f/2/f2269e80fdff75b328418cf84067a24aed769754_233013_2a.jpg",
+    link:
+      "https://www.technodom.kz/smartfony-i-gadzhety/smartfony-i-telefony/smartfony/p/smartfon-samsung-galaxy-a02s-32gb-blue-config/color-palette/chernihy/volume/32-gb"
+  },
+  {
+    id: 7,
+    name:
+      "LapTop Lenovo V14 Athlon Gold 3150U / 8Gb / 128SSD / 14 / Win10 / (82C6S03900)",
+    price: 409.7,
+    description:
+      "From viewing spreadsheets to video calls and multitasking, the Lenovo V14 (AMD) laptop supports everything you need. Powered by AMD technology, this low-cost 14-inch laptop is designed to help you get the job done while protecting your privacy. With built-in reliability features and a sleek, narrow-bezel design, this laptop is a reliable tool for your everyday professional needs",
+    rating: "4.7/5.0",
+    image:
+      "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/b/0/b086829503c0cd0264ffc2e8e02afaa74dedfa71_231330_1.jpg",
+    link:
+      "https://www.technodom.kz/noutbuki-i-komp-jutery/noutbuki-i-aksessuary/noutbuki/p/noutbuk-14-lenovo-v14-athlon-3150u-8-128-w-82c6s03900-231330"
+  },
+  {
+    id: 8,
+    name:
+      "LapTop Asus X515MA Celeron N4120 / 8Gb / 256SSD / 15,6 / DOS / (X515MA-EJ095)",
+    price: 361.5,
+    description:
+      "USB Type C, Card Reader, Слот для замка Kensington, USB 2.0, HDMI, Jack 3.5 mm, USB 3.2",
+    rating: "3.9/5.0",
+    image:
+      "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/d/4/d4cc6dee1a7f8c86b15da177eff8ef3a6f358d11_234164_1v.jpg",
+    link:
+      "https://www.technodom.kz/noutbuki-i-komp-jutery/noutbuki-i-aksessuary/noutbuki/p/noutbuk-asus-x515ma-celeron-n4120-8gb-256ssd-156-dos-234164"
+  },
+  {
+    id: 9,
+    name:
+      "LapTop Lenovo IdeaPad S340 i3 1005G1 / 8ГБ / 128SSD / 15.6 / DOS / (81VW00ATRK)",
+    price: 481.99,
+    description:
+      "The laptop with a diagonal of 15.6 (39.62 cm) has an integrated graphics card. This is a great alternative to a desktop computer.Suitable for various tasks - for business, study, work. The device is very popular due to its affordable price and good functionality. It will be a good replacement for a stationary PC - its memory capacity is quite enough to store the necessary data.In our online store ComputerMarket you can buy a laptop from the LENOVO company with a screen diagonal of 15.6 (39.62 cm). This model has the following technical characteristics: processor - Intel Core i3, hard disk capacity - 0 GB, RAM - 8 GB, screen resolution - 1920 x 1080.",
+    rating: "4.2/5.0",
+    image:
+      "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/6/3/6340990f62cc4a6ffa5b1afc3158e51f3a29e316_229761_2.jpg",
+    link:
+      "https://www.technodom.kz/uralsk/noutbuki-i-komp-jutery/noutbuki-i-aksessuary/noutbuki/p/noutbuk-lenovo-ideapad-s340-i3-1005g1-8gb-128ssd-229761"
+  },
+  {
+    id: 10,
+    name:
+      "Gaming LapTop Asus TUF Gaming F15 i5 10300H / 16ГБ / 512SSD / GTX1660Ti 6ГБ / 15.6 / DOS / (FX506LU-HN011)",
+    price: 1060.39,
+    description:
+      "The TUF Gaming F15 laptop can be equipped with a 144 Hz IPS screen, which is great for fast-paced PC games. When connected to an external monitor via USB 3.2 Gen2 Type-C in DisplayPort 1.4 mode, 120 Hz refresh rate, up to 4K resolution and G-Sync adaptive sync technology are supported, which eliminates the unpleasant frame-tearing effect. Another monitor or TV can be connected via HDMI 2.0b - it allows you to output 4K images with a refresh rate up to 60 Hz",
+    rating: "3.4/5.0",
+    image:
+      "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/7/4/747df0bc2e5ee2eb5e5979d9acd39a5cfe0712ba_230916a_2.jpg",
+    link:
+      "https://www.technodom.kz/uralsk/noutbuki-i-komp-jutery/noutbuki-i-aksessuary/noutbuki/p/igrovoj-noutbuk-asus-tuf-gaming-f15-fx506lu-hn011-230916"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
